40618ffb6fefdd74782f7174a35fa811080ee9df
screaminGreen <101007489+hiepfre3101@users.noreply.github.com>
2023-02-14 22:00:36 +0700

    Structure & config

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="bg-red-500 w-10 text-3xl font-bold">Hello world</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        primary: "#00C9B7",
+        primaryOpc1: "rgba(0,201,183,0.1)",
+        textWhite: "#96B1AC",
+        hightLightPink: "#E999DB",
+        backGround: "#344B47",
+      },
+      screens: {
+        xs: "480px",
+        ss: "620px",
+        sm: "768px",
+        md: "1060px",
+        lg: "1200px",
+        xl: "1700px",
+      },
+    },
+  },
+  plugins: [],
+};
